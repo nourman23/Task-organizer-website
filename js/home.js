895ad6d1dd50b27ee7307061f682a43abae8c5c9
khalledalkarmi <khalled_95@hotmail.com>
2022-08-24 17:19:17 +0300

    add home javaScript file to handle

---
+++ js/home.js
@@ -0,0 +1,19 @@
+
+let container = document.getElementById('container')
+
+toggle = () => {
+	container.classList.toggle('sign-in')
+	container.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+	container.classList.add('sign-in')
+}, 200)
+
+signInBtn.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
