96f555b0636b8c2506c6c9170a0289175fa15dc8
khalledalkarmi <khalled_95@hotmail.com>
2022-08-29 15:28:41 +0300

    finish dark mode

--- js/app.js
+++ js/app.js
@@ -87,11 +87,17 @@ function createCard(task, id) {
   let completeTask = document.createElement("span");
   completeTask.className = "input-group-text";
   completeTask.id = "inputGroup-sizing-default";
+  if (darkThemeFlag) {
+    completeTask.classList.add("completeDarkTheme")
+  }
   completeTask.textContent = "Complete";
   checkboxContaner.append(completeTask);
 
   let checkboxDiv = document.createElement("div");
   checkboxDiv.className = "input-group-text me-lg-3 me-md-3 me-sm-3 me-3 p-0";
+  if (darkThemeFlag) {
+    checkboxDiv.classList.add("completeDarkTheme")
+  }
   checkboxDiv.setAttribute(
     "style",
     "border-top-right-radius: 5px; border-bottom-right-radius: 5px;"
@@ -125,6 +131,10 @@ function createCard(task, id) {
   } else if (task.priority) {
     spanPriority.style.color = "blue";
     card.style.boxShadow = "0px 1px 14px 0px rgb(0 53 255 / 61%) ";
+  }
+
+  if (darkThemeFlag) {
+    spanPriority.classList.add("completeDarkTheme")
   }
 
   spanPriority.id = `priority-${id}`;
@@ -681,14 +691,14 @@ function calculateRemainTime(startDate, dateAsString) {
 }
 
 // display user name in aside bar for welcoming
-let welcomeName = document.getElementById("welcomeName");
+// let welcomeName = document.getElementById("welcomeName");
 
-for (let i = 0; i < user.length; i++) {
-  let element = user[i];
-  if (element.isLogged) {
-    welcomeName.textContent = `Welcome ${element.firstName}`;
-  }
-}
+// for (let i = 0; i < user.length; i++) {
+//   let element = user[i];
+//   if (element.isLogged) {
+//     welcomeName.textContent = `Welcome ${element.firstName}`;
+//   }
+// }
 //function to remove filter will use it before filter by state and priority
 function autoRemoveFilter() {
   without.click();
